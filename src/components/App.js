a42f7167d1374fa2514855ba28ef873b770ce0fc
Nick Cave <nxdmedia@googlemail.com>
2020-10-15 18:06:19 +0100

    Setup logic for requesting data from Flickr API

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // API Key Import
 import apiKey from '../config/config';
@@ -10,19 +10,43 @@ import Nav from './Nav';
 import NotFound from './NotFound';
 import SearchForm from './SearchForm';
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <SearchForm />
-        <Nav />
-        <Route exact path="/" component={PhotoContainer}/>
-        <Route exact path="/cats" component={ () => <PhotoContainer title="Cats"/>}/>
-        <Route exact path="/dogs" component={ () => <PhotoContainer title="Dogs"/>}/>
-        <Route exact path="/computers" component={ () => <PhotoContainer title="Computers"/>}/>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      images: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=sunsets&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => response.json())
+    .then(response => {
+      this.setState({images: response.photos.photo})
+    })
+    .catch(err => {
+      console.log('Error fetching and parsing data', err);
+    }); 
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <SearchForm />
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={ () => <PhotoContainer data={this.state.images}/>}/>
+            <Route path="/cats" component={ () => <PhotoContainer title="Cats"/>}/>
+            <Route path="/dogs" component={ () => <PhotoContainer title="Dogs"/>}/>
+            <Route path="/computers" component={ () => <PhotoContainer title="Computers"/>}/>
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
-
 import Photo from './Photo';
 
 class PhotoContainer extends Component {
+
   render() {
+    
+    let results = this.props.data;
+    let images = results.map(image => {
+      return <Photo key={image.id} src={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`} />
+    })
+
     return(
         <div className="photo-container">
             <h2>{this.props.title ? this.props.title : 'Results'}</h2>
             <ul>
-                <Photo />
-                <Photo />
-                <Photo />
-                <Photo />
+                {images}
             </ul>
         </div>
     );
